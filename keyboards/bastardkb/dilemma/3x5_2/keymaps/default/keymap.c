91a6ed04e6998f1cd60c01eca0e0c961f21cf3d7
André Costa <andreccosta@me.com>
2023-12-30 18:57:48 +0000

    fix: move kb info to assembled. fix keymap by disabling via

--- keyboards/bastardkb/dilemma/3x5_2/keymaps/default/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_2/keymaps/default/keymap.c
@@ -31,7 +31,7 @@ enum charybdis_keymap_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_split_3x5_2(
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_X,    KC_X,    KC_X,    KC_X,    KC_X,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
